c1f1299b9176aed761414ace56640cd946197a7b
Allen Spain <aspain2394@gmail.com>
2020-06-30 22:03:16 -0400

    Update auto_predict2.bash
    remove done

--- bbox_predict/auto_predict2.bash
+++ bbox_predict/auto_predict2.bash
@@ -7,7 +7,7 @@ for line in $(ls $search_path); do
 #     echo "$(dirname $line)"
     if [[ -d $line ]]; then 
         echo "$line is a Dir"
-        for filename in $line/*.png; do 
+        for filename in "$search_path/$line/*.png"; do 
             echo "$search_path/$(basename "$filename" .png).txt
             # time ./darknet detector test ./cfg/plant-weed-obj.data ./cfg/plant-weed-yolov3-tiny-r0-med.cfg ./evaluation/test_2015_med_r0/plant-weed-yolov3-tiny-r0-med_best.weights $filename -dont_show -ext_output >> "./data/test_video_frames/$filename/$(basename "$filename" .png).txt
     elif [[ -f $line ]]; then 
@@ -18,4 +18,4 @@ for line in $(ls $search_path); do
 done
     # make a prediction and store annotated image
     # cp ./predictions.jpg $line
-done
+
