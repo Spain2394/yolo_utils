33d996e0c36e884e23a431de574262eb3bae78d3
Allen Spain <aspain2394@gmail.com>
2020-06-30 22:06:51 -0400

    Update auto_predict2.bash

--- bbox_predict/auto_predict2.bash
+++ bbox_predict/auto_predict2.bash
@@ -8,7 +8,7 @@ for line in $(ls $search_path); do
     if [[ -d $line ]]; then 
         echo "$line is a Dir"
         for filename in "$search_path/$line/*.png"; do 
-            echo "$search_path/$(basename "$filename" .png).txt
+            echo "$search_path/$(basename "$filename" .png).txt"
             # time ./darknet detector test ./cfg/plant-weed-obj.data ./cfg/plant-weed-yolov3-tiny-r0-med.cfg ./evaluation/test_2015_med_r0/plant-weed-yolov3-tiny-r0-med_best.weights $filename -dont_show -ext_output >> "./data/test_video_frames/$filename/$(basename "$filename" .png).txt
     elif [[ -f $line ]]; then 
         echo "$not a directory" 
