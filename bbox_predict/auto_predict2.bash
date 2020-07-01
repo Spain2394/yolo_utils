c15f74ebacc8e7e4d38a9f893104f406ceb3621e
Allen Spain <aspain2394@gmail.com>
2020-06-30 22:07:02 -0400

    Update auto_predict2.bash

--- bbox_predict/auto_predict2.bash
+++ bbox_predict/auto_predict2.bash
@@ -3,8 +3,6 @@
 # old usage bash auto_predict.bash < predict_target.txt
 search_path = ./data/test_video_frames
 for line in $(ls $search_path); do
-    # time ./darknet detector test ./cfg/plant-weed-obj.data ./cfg/plant-weed-yolov3-tiny-r0-med.cfg ./evaluation/test_2015_med_r0/plant-weed-yolov3-tiny-r0-med_best.weights $line -dont_show -ext_output >> "./data/test_video_frames/$(basename "$line" .png).txt"
-#     echo "$(dirname $line)"
     if [[ -d $line ]]; then 
         echo "$line is a Dir"
         for filename in "$search_path/$line/*.png"; do 
