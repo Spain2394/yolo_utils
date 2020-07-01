96ea0c9348d16db83b1570e854bcaab887a6f7a8
Allen Spain <aspain2394@gmail.com>
2020-06-30 22:02:08 -0400

    Update auto_predict2.bash
    update

--- bbox_predict/auto_predict2.bash
+++ bbox_predict/auto_predict2.bash
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-while read line; do 
-    # echo $(dirname "$line")
-    # echo $(basename "$line" .jpg).txt
-    # echo hello >> $(dirname "$line")"/"$(basename "$line" .jpg).txt
-    echo "file path: $line"
-    echo "writting to $(dirname "$line")"/"$(basename "$line" .jpg).txt"
-    time ./darknet detector test ./cfg/plant-weed-obj.data ./cfg/plant-weed-yolov3-tiny-r0.cfg ./traint-weed-yolov3-tiny-r0_final.weights $line -dont_show -ext_output >> $(dirname "$line")"/"$(basename "$line" .jpg).txt
+# old usage bash auto_predict.bash < predict_target.txt
+search_path = ./data/test_video_frames
+for line in $(ls $search_path); do
+    # time ./darknet detector test ./cfg/plant-weed-obj.data ./cfg/plant-weed-yolov3-tiny-r0-med.cfg ./evaluation/test_2015_med_r0/plant-weed-yolov3-tiny-r0-med_best.weights $line -dont_show -ext_output >> "./data/test_video_frames/$(basename "$line" .png).txt"
+#     echo "$(dirname $line)"
+    if [[ -d $line ]]; then 
+        echo "$line is a Dir"
+        for filename in $line/*.png; do 
+            echo "$search_path/$(basename "$filename" .png).txt
+            # time ./darknet detector test ./cfg/plant-weed-obj.data ./cfg/plant-weed-yolov3-tiny-r0-med.cfg ./evaluation/test_2015_med_r0/plant-weed-yolov3-tiny-r0-med_best.weights $filename -dont_show -ext_output >> "./data/test_video_frames/$filename/$(basename "$filename" .png).txt
+    elif [[ -f $line ]]; then 
+        echo "$not a directory" 
+    else 
+        echo "Invalid path" 
+    fi
 done
-
+    # make a prediction and store annotated image
+    # cp ./predictions.jpg $line
+done
